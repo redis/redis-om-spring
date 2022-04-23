eaf6bd0c6f26bbecb026a5f822133ff75d5caddb
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-04-22 19:13:06 -0700

    refactor: remove dead code

--- redis-om-spring/src/main/java/com/redis/om/spring/annotations/Nested.java
+++
@@ -1,9 +0,0 @@
-package com.redis.om.spring.annotations;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.FIELD, ElementType.ANNOTATION_TYPE })
-public @interface Nested {
-}
