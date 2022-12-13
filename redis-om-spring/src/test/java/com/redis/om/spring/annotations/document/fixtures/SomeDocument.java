db64af822204c7079e1f0aa5295049a888fe126a
Brian Sam-Bodden <bsb@redis.com>
2022-12-12 21:43:35 -0700

    fix: Resolve GSON MalformedJsonException when returning selected String values using Entity Streams (resolves gh-124)

--- redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/SomeDocument.java
+++ redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/SomeDocument.java
@@ -1,2 +1,56 @@
-package com.redis.om.spring.annotations.document.fixtures;public class SomeDocument {
+package com.redis.om.spring.annotations.document.fixtures;
+
+import com.redis.om.spring.annotations.*;
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+
+@Data @NoArgsConstructor @Document public class SomeDocument {
+  @NonNull @Id @Indexed protected String id;
+  @NonNull @Indexed private String name;
+
+  @Indexed(sortable = true) private LocalDateTime documentCreationDate;
+  @Searchable private String description;
+  @Indexed private Source source;
+  @Searchable private String category;
+  @Indexed private Format format;
+  @Searchable private String objectStorageKey;
+  @Searchable private String searchableContent;
+
+  public enum Format {
+    pdf,
+    word,
+    text,
+    png,
+    jpeg
+  }
+
+  public enum Source {
+    sourceA,
+    sourceB
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+
+    SomeDocument that = (SomeDocument) o;
+
+    if (!id.equals(that.id))
+      return false;
+    return name.equals(that.name);
+  }
+
+  @Override public int hashCode() {
+    int result = id.hashCode();
+    result = 31 * result + name.hashCode();
+    return result;
+  }
+
 }
--- redis-om-spring/src/test/java/com/redis/om/spring/search/stream/EntityStreamsIssuesTest.java
+++ redis-om-spring/src/test/java/com/redis/om/spring/search/stream/EntityStreamsIssuesTest.java
@@ -1,39 +1,56 @@
 package com.redis.om.spring.search.stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.redis.om.spring.AbstractBaseDocumentTest;
 import com.redis.om.spring.annotations.document.fixtures.SomeDocument;
-import com.redis.om.spring.annotations.document.fixtures.SomeDocument.Format;
-import com.redis.om.spring.annotations.document.fixtures.SomeDocument.Source;
+import com.redis.om.spring.annotations.document.fixtures.SomeDocument$;
 import com.redis.om.spring.annotations.document.fixtures.SomeDocumentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class EntityStreamsIssues extends AbstractBaseDocumentTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EntityStreamsIssuesTest extends AbstractBaseDocumentTest {
+  @Autowired GsonBuilder gsonBuilder;
   @Autowired SomeDocumentRepository someDocumentRepository;
 
-  @BeforeEach
-  void beforeEach() {
-    someDocumentRepository.deleteAll();
+  @Autowired EntityStream entityStream;
 
-    SomeDocument sd1 = new SomeDocument();
-    sd1.setCategory("CAT1");
-    sd1.setDescription("This is the description");
-    sd1.setFormat(Format.pdf);
-    sd1.setName("SomeDocument1");
-    sd1.setSource(Source.sourceA);
-    sd1.setDocumentCreationDate(LocalDateTime.now());
-    sd1.setSearchableContent("This is the searchable content");
-    someDocumentRepository.save(sd1);
+  @BeforeEach void beforeEach() throws IOException {
+    // Load Sample Docs
+    if (someDocumentRepository.count() == 0) {
+      Reader reader = null;
+      try {
+        reader = Files.newBufferedReader(Paths.get("src/test/resources/data/some_documents.json"));
+        List<SomeDocument> characterEntries = gsonBuilder.create()
+            .fromJson(reader, new TypeToken<List<SomeDocument>>() {}.getType());
+        someDocumentRepository.saveAll(characterEntries);
+      } catch (Exception ex) {
+        throw ex;
+      } finally {
+        reader.close();
+      }
+    }
   }
 
-  @Test
-  void printTheJSON() {
+  // issue gh-124 - return fields of type String with target String cause GSON MalformedJsonException
+  @Test void testReturnFieldsOfTypeStringAreProperlyReturned() {
     var docs = someDocumentRepository.findAll();
-    assertThat(docs).hasSize(1);
+    List<String> results = entityStream.of(SomeDocument.class) //
+        .filter(SomeDocument$.NAME.eq("LRAWMRENZY")) //
+        .limit(1000) //
+        .map(SomeDocument$.DESCRIPTION) //
+        .collect(Collectors.toList());
+    results.stream().forEach(System.out::println);
+    assertThat(results).contains("nsw fifth pens geo buffalo");
   }
 }
