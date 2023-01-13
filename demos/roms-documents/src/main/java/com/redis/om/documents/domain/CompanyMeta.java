bd74dc3af711055e8b4c7b1616837d61ad1ce988
Francesco Distefano <f.distefano@synapseslab.com>
2023-01-13 08:42:05 +0100

    Add CompanyMeta to test subindex capabilities

---
+++ demos/roms-documents/src/main/java/com/redis/om/documents/domain/CompanyMeta.java
@@ -0,0 +1,30 @@
+package com.redis.om.documents.domain;
+
+import java.util.Set;
+
+import com.redis.om.spring.annotations.Document;
+import com.redis.om.spring.annotations.Indexed;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor(staticName = "of")
+@Document
+public class CompanyMeta {
+    
+    @Indexed
+    @NonNull
+    private String stringValue;
+
+    @Indexed
+    @NonNull
+    private Integer numberValue;
+
+    @Indexed
+    @NonNull
+    private Set<String> tagValues;
+}
