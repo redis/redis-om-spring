1f1b40d22c2f189ac9a16e52d67081ab766fdf99
Brian Sam-Bodden <bsb@redis.com>
2023-02-08 17:28:54 -0700

    fix: resolve .filter(freetext) order independent (resolves gh-176)

--- redis-om-spring/src/test/java/com/redis/om/spring/search/stream/EntityStreamsIssuesTest.java
+++ redis-om-spring/src/test/java/com/redis/om/spring/search/stream/EntityStreamsIssuesTest.java
@@ -5,16 +5,21 @@ import com.redis.om.spring.annotations.document.fixtures.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SuppressWarnings("SpellCheckingInspection") class EntityStreamsIssuesTest extends AbstractBaseDocumentTest {
   @Autowired SomeDocumentRepository someDocumentRepository;
   @Autowired DeepNestRepository deepNestRepository;
+  @Autowired DocRepository docRepository;
 
   @Autowired EntityStream entityStream;
 
@@ -58,5 +63,30 @@ import static org.assertj.core.api.Assertions.assertThat;
         .map(DeepNest$.NAME) //
         .collect(Collectors.toList());
     assertThat(results).containsOnly("dn-3");
+  }
+
+  // issue gh-176
+  @Test
+  void testFreeFormTextSearchOrderIssue() {
+    docRepository.deleteAll();
+    Doc redis1 = docRepository.save(Doc.of("Redis", "wwwabccom"));
+    Doc redis2 = docRepository.save(Doc.of("Redis", "wwwxyznet"));
+    Doc microsoft1 = docRepository.save(Doc.of("Microsoft", "wwwabcnet"));
+    Doc microsoft2 = docRepository.save(Doc.of("Microsoft", "wwwxyzcom"));
+
+    var withFreeTextFirst = entityStream.of(Doc.class)
+        .filter("*co*")
+        .filter(Doc$.FIRST.eq("Microsoft"))
+        .collect(Collectors.toList());
+
+    var withFreeTextLast = entityStream.of(Doc.class)
+        .filter(Doc$.FIRST.eq("Microsoft"))
+        .filter("*co*")
+        .collect(Collectors.toList());
+
+    assertAll( //
+        () -> assertThat(withFreeTextLast).containsExactly(microsoft2),
+        () -> assertThat(withFreeTextFirst).containsExactly(microsoft2)
+    );
   }
 }
