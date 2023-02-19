77c29053c69df052ac0b526b24ad40eb1959fc9c
khushwinder <chahalkhushwinder@gmail.com>
2023-02-19 16:04:32 -0500

    OffsetDateTime converters for Hashes

---
+++ redis-om-spring/src/main/java/com/redis/om/spring/convert/BytesToOffsetDateTimeConverter.java
@@ -0,0 +1,23 @@
+package com.redis.om.spring.convert;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+@ReadingConverter
+public class BytesToOffsetDateTimeConverter implements Converter<byte[], OffsetDateTime> {
+
+  @Override
+  public OffsetDateTime convert(byte[] source) {
+    return OffsetDateTime.ofInstant(Instant.ofEpochSecond(Long.parseLong(toString(source))), ZoneId.systemDefault());
+  }
+
+  String toString(byte[] source) {
+    return new String(source, StandardCharsets.UTF_8);
+  }
+
+}
