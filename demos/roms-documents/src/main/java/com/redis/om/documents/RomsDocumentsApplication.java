242a4792af3a07079f3e12dabf05b0989031e57e
Brian Sam-Bodden <bsb@redis.com>
2023-03-20 22:16:40 -0700

    refactor: removed unnecessary code from demo

--- demos/roms-documents/src/main/java/com/redis/om/documents/RomsDocumentsApplication.java
+++ demos/roms-documents/src/main/java/com/redis/om/documents/RomsDocumentsApplication.java
@@ -1,22 +1,19 @@
 package com.redis.om.documents;
 
-import java.util.Arrays;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.geo.Point;
-
 import com.redis.om.documents.domain.Company;
 import com.redis.om.documents.domain.CompanyMeta;
 import com.redis.om.documents.domain.Person;
 import com.redis.om.documents.repositories.CompanyRepository;
 import com.redis.om.documents.repositories.PersonRepository;
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.geo.Point;
+
+import java.util.Set;
 
 @SpringBootApplication
 @EnableRedisDocumentRepositories
@@ -27,20 +24,6 @@ public class RomsDocumentsApplication {
 
   @Autowired
   PersonRepository personRepo;
-  
-  @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    return args -> {
-
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
-    };
-  }    
 
   @Bean
   CommandLineRunner loadTestData() {
