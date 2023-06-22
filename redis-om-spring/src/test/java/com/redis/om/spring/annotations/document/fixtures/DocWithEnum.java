1185022bc56437a0f03da43bc503a7f9673b1ff9
Brian Sam-Bodden <bsb@redis.com>
2023-06-22 04:10:08 -0700

    fix: no metamodel tag fields generated for @Indexed Enums (resolves gh-277)

--- redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/DocWithEnum.java
+++ redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/DocWithEnum.java
@@ -2,6 +2,7 @@ package com.redis.om.spring.annotations.document.fixtures;
 
 import com.redis.om.spring.annotations.Document;
 import com.redis.om.spring.annotations.Indexed;
+import com.redis.om.spring.annotations.Searchable;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
@@ -17,4 +18,8 @@ public class DocWithEnum {
   @Indexed
   @NonNull
   private MyJavaEnum enumProp;
+
+  @Searchable
+  @NonNull
+  private MyJavaEnum searchableEnumProp;
 }
