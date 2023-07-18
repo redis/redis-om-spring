2c0548c17044665e8b3a8c21934844b9b7c4ace8
Brian Sam-Bodden <bsb@redis.com>
2023-07-18 12:32:39 -0700

    fix: support deep nested fields updateField in DocumentsRedisRepositories (resolves gh-305)

--- redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/DeepNestRepository.java
+++ redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/DeepNestRepository.java
@@ -2,6 +2,8 @@ package com.redis.om.spring.annotations.document.fixtures;
 
 import com.redis.om.spring.repository.RedisDocumentRepository;
 
-@SuppressWarnings("unused") public interface DeepNestRepository extends RedisDocumentRepository<DeepNest, String> {
+import java.util.Optional;
 
+@SuppressWarnings("unused") public interface DeepNestRepository extends RedisDocumentRepository<DeepNest, String> {
+  Optional<DeepNest> findFirstByNameIs(String name);
 }
