cfe5c000b71a0b50d9cfd6c292a6f561d2b52b9f
Brian Sam-Bodden <bsb@redis.com>
2023-08-24 21:18:29 -0700

    test: add FT.CREATE filter tests for @Document

---
+++ redis-om-spring/src/test/java/com/redis/om/spring/annotations/document/fixtures/VehicleType.java
@@ -0,0 +1,13 @@
+package com.redis.om.spring.annotations.document.fixtures;
+
+public enum VehicleType {
+  SEDAN,
+  COUPE,
+  SPORTS_CAR,
+  STATION_WAGON,
+  HATCHBACK,
+  CONVERTIBLE,
+  SUV,
+  MINIVAN,
+  PICKUP_TRUCK
+}
