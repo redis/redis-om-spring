ca3683e7df2108e9ebff8b197fdbf132f9c8c348
Gediminas Morkys <gediminas.morkys@teliacompany.com>
2023-09-22 17:09:00 +0300

    test: test filtering by nested list properties

---
+++ redis-om-spring/src/test/java/com/redis/om/spring/fixtures/document/model/Scheduled.java
@@ -0,0 +1,25 @@
+package com.redis.om.spring.fixtures.document.model;
+
+import com.redis.om.spring.annotations.Document;
+import com.redis.om.spring.annotations.Indexed;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Document
+@Data
+@RequiredArgsConstructor(staticName = "of")
+public class Scheduled {
+
+    @Id
+    @NonNull
+    private String id;
+
+    @Indexed
+    @NonNull
+    private List<Filter> filters;
+
+}
