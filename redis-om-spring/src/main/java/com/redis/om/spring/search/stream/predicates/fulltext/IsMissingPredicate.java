ad1f007d551d49b400f8e8d4f1756e80066af201
Raphael De Lio <raphadlts@gmail.com>
2026-02-14 12:22:27 +0100

    fix: correct isMissing predicate behavior with sequential filters

--- redis-om-spring/src/main/java/com/redis/om/spring/search/stream/predicates/fulltext/IsMissingPredicate.java
+++ redis-om-spring/src/main/java/com/redis/om/spring/search/stream/predicates/fulltext/IsMissingPredicate.java
@@ -4,6 +4,7 @@ import com.redis.om.spring.metamodel.SearchFieldAccessor;
 import com.redis.om.spring.search.stream.predicates.BaseAbstractPredicate;
 
 import redis.clients.jedis.search.querybuilder.Node;
+import redis.clients.jedis.search.querybuilder.QueryBuilders;
 
 /**
  * Represents an "is missing" predicate for field searches.
@@ -29,7 +30,7 @@ public class IsMissingPredicate<E, T> extends BaseAbstractPredicate<E, T> {
   public Node apply(Node root) {
     String query = String.format("ismissing(@%s)", getSearchAlias());
 
-    return new Node() {
+    Node isMissingNode = new Node() {
       @Override
       public String toString() {
         return query;
@@ -40,5 +41,9 @@ public class IsMissingPredicate<E, T> extends BaseAbstractPredicate<E, T> {
         return query;
       }
     };
+
+    // Combine with the existing root using AND (intersect)
+    // If root is empty, just return the isMissing node
+    return root.toString().isBlank() ? isMissingNode : QueryBuilders.intersect(root, isMissingNode);
   }
 }
